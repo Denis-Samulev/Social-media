03d0aab5c01260a661e51a1815cd321377d7c3b0
Denis Samulev <d.samulev@bk.ru>
2020-07-02 20:57:22 +0300

    'store,refactor'

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import state, {subscribe} from "./redux/state";
+// import state, {subscribe} from "./redux/state";
+import store from "./redux/state";
 import ReactDOM from "react-dom";
 import React from "react";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
-import {addPost, updateNewPostText} from "./redux/state";
+// import {addPost, updateNewPostText} from "./redux/state";
 
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+                <App state={state}
+                     addPost={store.addPost.bind(store)}
+                     updateNewPostText={store.updateNewPostText.bind(store)}/>
             </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
@@ -20,8 +23,8 @@ let rerenderEntireTree = (state) => {
 }
 
 
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 
 
 // If you want your app to work offline and load faster, you can change
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,64 +1,65 @@
-let rerenderEntireTree = () => {
-    console.log('State changed')
-}
+let store = {
+    _state: {
 
+        profilePage: {
+            postData: [
+                {id: 1, message: 'It is my first post', like:'10'},
+                {id: 2, message: 'It is important to be happy', like:'20'},
+                {id: 3, message: 'It is important to be happy', like:'30'},
+                {id: 4, message: 'It is important to be happy', like:'15'},
+                {id: 5, message: 'It is important to be happy', like:'15'},
+                {id: 6, message: 'It is important ', like:'15'}
+            ],
+            newPostText: 'it-kamasutra'
+        },
 
-let state = {
-
-    profilePage: {
-        postData: [
-            {id: 1, message: 'It is my first post', like:'10'},
-            {id: 2, message: 'It is important to be happy', like:'20'},
-            {id: 3, message: 'It is important to be happy', like:'30'},
-            {id: 4, message: 'It is important to be happy', like:'15'},
-            {id: 5, message: 'It is important to be happy', like:'15'},
-            {id: 6, message: 'It is important ', like:'15'}
-        ],
-        newPostText: 'it-kamasutra'
+        dialogPage: {
+            messagesData: [
+                {id: 1, message: 'Lorem ipsum dolor.'},
+                {id: 2, message: 'Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor.'},
+                {id: 3, message: 'Lorem ipsum dolor.'},
+                {id: 4, message: 'Lorem ipsum dolor orem ipsum dolor.'},
+                {id: 5, message: 'Lorem ipsum dolor orem ipsum dolor lorem ipsum dolor.'},
+                {id: 6, message: 'Lorem ipsum dolor.'}
+            ],
+            dialogsData: [
+                {id: 1, name: 'Andrew'},
+                {id: 2, name: 'Dmitry'},
+                {id: 3, name: 'Sasha'},
+                {id: 4, name: 'Sveta'},
+                {id: 5, name: 'Valera'},
+                {id: 6, name: 'Viktor'}
+            ]
+        }
     },
-
-    dialogPage: {
-        messagesData: [
-            {id: 1, message: 'Lorem ipsum dolor.'},
-            {id: 2, message: 'Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor.'},
-            {id: 3, message: 'Lorem ipsum dolor.'},
-            {id: 4, message: 'Lorem ipsum dolor orem ipsum dolor.'},
-            {id: 5, message: 'Lorem ipsum dolor orem ipsum dolor lorem ipsum dolor.'},
-            {id: 6, message: 'Lorem ipsum dolor.'}
-        ],
-        dialogsData: [
-            {id: 1, name: 'Andrew'},
-            {id: 2, name: 'Dmitry'},
-            {id: 3, name: 'Sasha'},
-            {id: 4, name: 'Sveta'},
-            {id: 5, name: 'Valera'},
-            {id: 6, name: 'Viktor'}
-        ]
+    getState () {
+        return this._state;
+    },
+    _callSubscriber () {
+        console.log('State changed')
+    },
+    addPost () {
+        let newPost = {
+            id: 7,
+            message: this._state.profilePage.newPostText,
+            like: 0
+        };
+        this._state.profilePage.postData.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber (this._state);
+    },
+    updateNewPostText (newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber (this._state);
+    },
+    subscribe (observer) {
+        this._callSubscriber = observer;
     }
 }
 
 
-export const addPost = () => {
-    let newPost = {
-        id: 7,
-        message: state.profilePage.newPostText,
-        like: 0
-    };
-    state.profilePage.postData.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree (state);
-}
 
 
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree (state);
-}
 
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-
-export default state;
+export default store;
+window.store = store;
